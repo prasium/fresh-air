45c1c67997ab82732017dfbf25f171f738cafe20
lenovo <dikshagoyal03042000@gmail.com>
2022-04-09 21:57:25 +0530

    Error, not working

---
+++ lib/screens/loginScreen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_air/utils/authentication.dart';
+import 'package:fresh_air/widgets/googleSignInButton.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Image.asset(
+                        'assets/firebase_logo.png',
+                        height: 160,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Fresh Air',
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                    Text(
+                      'Authentication',
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FutureBuilder(
+                future: Authentication.initializeFirebase(context: context),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error initializing Firebase');
+                  } else if (snapshot.connectionState == ConnectionState.done) {
+                    return GoogleSignInButton();
+                  }
+                  return CircularProgressIndicator();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/screens/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_air/screens/loginScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FreshAir',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF4B4FBE),
+        primarySwatch: Colors.indigo,
+        accentColor: Color(0xFF0C0C7A)
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
