7f6ff2b7731c7e78c0eb6018feab6eaa3098c27e
lenovo <dikshagoyal03042000@gmail.com>
2022-04-18 22:49:43 +0530

    UI changes

--- lib/widgets/aqcard.dart
+++ lib/widgets/aqcard.dart
@@ -1,62 +1,159 @@
 import 'package:flutter/material.dart';
+import 'package:weather_icons/weather_icons.dart';
 
 class AQCard extends StatelessWidget {
   final temp, humid, pm25;
+
   const AQCard({Key? key, this.temp, this.humid, this.pm25}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-      height: 300,
-        child: Card(
-          child: ListTile(
-            title: Stack(
-              children:[
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Text(temp + ' Degree Celsius '),
-                        SizedBox(height: 10),
-                        Text(humid + '% RH '),
-                      ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                  right: 60,
-                  bottom: 10,
-                  child: Card(
-                    child: Container(
-                      alignment: Alignment.bottomRight,
-                      height: 160,
-                      width: 160,
-                      child: Center(
-                        child: ListTile(
-                          title: Text(pm25 + " ug/m^3 "),
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: size.width,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  color: Colors.indigo,
+                  elevation: 3,
+                  margin: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "PM2.5 Concentration",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Expanded(
+                        child: Card(
+                          child: Center(
+                              child: Text(
+                            pm25 + " ug/m^3 ",
+                            style: TextStyle(fontSize: 20),
+                          ),),
+                          elevation: 5,
+                          shadowColor: Colors.white38,
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 80),
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.white, width: 1),
+                            borderRadius: BorderRadius.circular(150),
+                          ),
                         ),
                       ),
-                    ),
-                    elevation: 8,
-                    shadowColor: Colors.green,
-                    margin: EdgeInsets.all(20),
-                    shape: CircleBorder(side: BorderSide(width: 1, color: Colors.white),
-                    ),
+                    ],
                   ),
                 ),
-              ] ,
+              ),
             ),
-          ),
-          elevation: 8,
-          shadowColor: Colors.green,
-          margin: EdgeInsets.all(20),
-          shape:  OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(color: Colors.white)
-          ),
-        )
+            Expanded(
+              child: Container(
+                width: size.width,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  color: Colors.indigo,
+                  elevation: 3,
+                  margin: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Meteorological Factors",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Expanded(
+                        child: Container(
+                          width: size.width,
+                          child: Card(
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    WeatherIcons.thermometer,
+                                    color: Color(0xFF4B4FBE),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    temp + " \u2103 ",
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            elevation: 5,
+                            shadowColor: Colors.white38,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 25, horizontal: 30),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(color: Colors.white, width: 1),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          width: size.width,
+                          child: Card(
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    WeatherIcons.humidity,
+                                    color: Color(0xFF4B4FBE),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    humid + " %RH ",
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            elevation: 5,
+                            shadowColor: Colors.white38,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 25, horizontal: 30),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(color: Colors.white, width: 1),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
